ff9f4f9c2854626fd06a3f45537197cd4b98ccb8
Rohit Kumar <roxtar@gmail.com>
2011-07-19 08:31:05 -0700

    - Started flot integration. - Changed panel size - Placeholder in place to show bar charts

--- data/render.js
+++ data/render.js
@@ -1,11 +1,19 @@
 self.port.on('render', function(store) { 
 	var str = " " ;
+	var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+	var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
 	for(var date in store) {
-	    for(var url in store[date]) {
-		str += url + " ";
-		str += store[date][url];
-		str += "<br/>";
-	    }
-	}
-	document.body.innerHTML = str;
+	    $.plot($("#placeholder"), [{
+			data: store[date],
+			lines: {show: true, fill: true}
+		    }]);
+
+	} 
+	    // for(var url in store[date]) {
+	    // 	str += url + " ";
+	    // 	str += store[date][url];
+	    // 	str += "<br/>";
+	    // }
+	  //}
+	  //document.body.innerHTML = str;
     });
