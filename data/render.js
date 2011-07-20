94bdba3b88a9b3c16857e74d16a371adacdad33b
Rohit Kumar <roxtar@gmail.com>
2011-07-19 20:25:37 -0700

    - Added basic bar graph to display statistics. - Uses flot stack which shouldn't be required.

--- data/render.js
+++ data/render.js
@@ -1,19 +1,22 @@
 self.port.on('render', function(store) { 
-	var str = " " ;
-	var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
-	var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+	var data = [];
+	var i = 0;
 	for(var date in store) {
-	    $.plot($("#placeholder"), [{
-			data: store[date],
-			lines: {show: true, fill: true}
-		    }]);
+	    for(var url in store[date]) { 
+		data.push(
+			  {
+			      label: url, 
+				  data: [[i,parseInt(store[date][url])]]
+				  }	
+			  );
+		i++;
+	    }
+	  }
+	var options = {
+	    series: {stack:0,
+		     lines: {show:false, steps:false},
+		     bars:{show:true}}
+	};
 
-	} 
-	    // for(var url in store[date]) {
-	    // 	str += url + " ";
-	    // 	str += store[date][url];
-	    // 	str += "<br/>";
-	    // }
-	  //}
-	  //document.body.innerHTML = str;
+	$.plot($("#placeholder"), data, options);	
     });
