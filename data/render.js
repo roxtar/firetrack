dfe6608140c4b0bfc9b73ad4fcc0a1fbe808eefe
Rohit Kumar <roxtar@gmail.com>
2011-08-07 08:25:55 -0700

    Try to fix google search results bug.
    - There is a bug either in firetrack or somewhere else which causes the
      google search results to not open up properly. If I type in a search query and
      click on a result, I am redirected back to the Google home page. This doesn't
      happen for Bing though.
    
    - I have changed the "div" id tags of the panel and the tooltip to have a "ft"
      prefix. This hasn't solved the problem though

--- data/render.js
+++ data/render.js
@@ -1,12 +1,11 @@
-self.port.on('render', function(store) {
-
+self.port.on('render', function (store) {	
+	"use strict";
 	// reads the time data present in "store"
 	// and renders it as a bar graph using
 	// flot
-	var data = new Array();
-	var i = 0;
-	for(var date in store) {
-	    for(var url in store[date]) {
+	var data = [], i = 0, date, url, options;
+	for (date in store) {
+	    for (url in store[date]) {
 		data.push(
 			  {
 			      label: url,
@@ -17,7 +16,7 @@ self.port.on('render', function(store) {
 	    }
 	  }
 
-	var options = {
+	options = {
 	    series: {stack:0,
 		     lines: {show:false, steps:false},
 		     bars:{show:true}},
@@ -26,10 +25,10 @@ self.port.on('render', function(store) {
 	    },
 	    grid: {hoverable: true},
 	};
-	$.plot($("#placeholder"), data, options);
+	$.plot($("#ft_panel"), data, options);
 
 	function showTooltip(x, y, contents) {
-	    $('<div id="tooltip">' + contents + '</div>').css( {
+	    $('<div id="ft_tooltip">' + contents + '</div>').css( {
 		    position: 'absolute',
 			display: 'none',
 			top: y - 30,
@@ -42,11 +41,11 @@ self.port.on('render', function(store) {
 	}
 
 	var previousLabel = null;
-	$("#placeholder").bind("plothover", function (event, pos, item) {
+	$("#ft_panel").bind("plothover", function (event, pos, item) {
 		    if (item) {
 			if (previousLabel != item.series.label) {
 			    previousLabel = item.series.label;
-			    $("#tooltip").remove();
+			    $("#ft_tooltip").remove();
 			    var x = item.datapoint[0].toFixed(2),
 				y = item.datapoint[1].toFixed(2);
 
@@ -55,7 +54,7 @@ self.port.on('render', function(store) {
 			}
 		    }
 		    else {
-			$("#tooltip").remove();
+			$("#ft_tooltip").remove();
 			previousLabel = null;
 		    }
 	    });
